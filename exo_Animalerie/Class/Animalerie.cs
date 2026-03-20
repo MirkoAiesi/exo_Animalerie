06b40aa00d1b105f0ba7223771789b0a3ae5fb7a
Aiesi Mirko <m.aiesi@stag.technofuturtic.education>
2026-03-20 16:38:39 +0100

    Ajout de la procédure CountAnimal

--- exo_Animalerie/Class/Animalerie.cs
+++ exo_Animalerie/Class/Animalerie.cs
@@ -3,6 +3,9 @@
 public class Animalerie
 {
     private List<Animal> Liste = new List<Animal>();
+    public int NbrChien { get; set; }
+    public int NbrChat { get; set; }
+    public int NbrOiseau { get; set; }
 
     public void AddAnimal(Animal animal)
     {
@@ -21,7 +24,34 @@ public class Animalerie
             {
                 Console.WriteLine($"{ca.Nom} il est très {ca.Caractere}, il a {ca.Age} ans ce qui fait {ca.AgeHumain} ans en humain, il a les griffes {(ca.CoupeGriffe ? "coupées" : "non-coupées")}" );
             }
+            else if (n is Oiseau o)
+            {
+                Console.WriteLine($"{o.Nom} il est de couleur {o.Couleur}, il a {o.Age} ans ce qui fait {o.AgeHumain} ans en humain, il est enfermé dans une {o.Habitat}" );
+            }
             
         }
     }
+
+    public void CountAnimal()
+    {
+        foreach (Animal a in Liste)
+        {
+            switch (a)
+            {
+                case Chien c:
+                    NbrChien++;
+                    break;
+                case Chat ca:
+                    NbrChat++;
+                    break;
+                case Oiseau o:
+                    NbrOiseau++;
+                    break;
+            }
+        }
+        Console.WriteLine($"Il ya {NbrChien} chien");
+        Console.WriteLine($"Il ya {NbrChat} chat");
+        Console.WriteLine($"Il ya {NbrOiseau} oiseau");
+    }
+    
 }
--- exo_Animalerie/Program.cs
+++ exo_Animalerie/Program.cs
@@ -5,9 +5,11 @@ Animalerie animalerie = new Animalerie();
 Animal chien = new Chien("Toby", 60, 72, "male", 2, new DateTime(2024, 03, 14), EnumCollier.bleu, "Dresser", EnumRace.Rotweiler);
 Animal chien2 = new Chien("Douki", 8, 30, "male", 6, new DateTime(2020, 03, 14), EnumCollier.bleu, "Dresser", EnumRace.Rotweiler);
 Animal chat = new Chat("Garfield", 10, 50, "male", 10, new DateTime(2016, 07, 18), EnumCaractere.farouche , true, true );
-//Animal oiseau = new Oiseau();
+Animal oiseau = new Oiseau("Morgan", 6, 38, "male", 14, new DateTime(2012, 07, 18), EnumCouleurOiseau.multicouleur, EnumHabitat.volière );
 
 animalerie.AddAnimal(chien);
 animalerie.AddAnimal(chien2);
 animalerie.AddAnimal(chat);
+animalerie.AddAnimal(oiseau);
+animalerie.CountAnimal();
 animalerie.ShowAnimal();
